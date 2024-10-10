1b6caefee18af321305d28af29fdf66409c8c1d5
Youssef Shawki <ya0721435@gmail.com>
2024-10-10 18:28:56 +0300

    report alignment results as txt file

--- Needleman-Wunsch/visual_text_reporting.py
+++ Needleman-Wunsch/visual_text_reporting.py
@@ -1,7 +1,42 @@
 import matplotlib.pyplot as plt
+import os
 from NW import *
 
-def plot_needleman_wunsch(protein1, protein2, scoring_scheme):
+
+def save_alignment_to_file(protein1, protein2, paths, optimal_score, file_name):
+    """
+    Save the alignment result to a file.
+
+    Parameters:
+    protein1 (str): The first protein sequence.
+    protein2 (str): The second protein sequence.
+    path (list): The optimal alignment path.
+    optimal_score (int): The alignment score. 
+    file_name (str): The name of the file to save the alignment.
+    """
+    if os.path.exists(file_name):
+        os.remove(file_name)
+
+    for ind, path in enumerate(paths):
+        aligned_protein1 = []
+        aligned_protein2 = []
+        i_cache, j_cache = path[-1]
+        for (i, j) in list(reversed(path))[1:]: # neglect for caching(0, 0)
+            if i == i_cache and j != j_cache: # moved horizontally
+                aligned_protein1.append('-')
+                aligned_protein2.append(protein2[j - 1])  
+            elif i != i_cache and j == j_cache: # moved vertically
+                aligned_protein1.append(protein1[i - 1])
+                aligned_protein2.append('-')
+            elif i != i_cache and j != j_cache: # moved diagonally 
+                aligned_protein1.append(protein1[i - 1])
+                aligned_protein2.append(protein2[j - 1])
+            i_cache, j_cache = i, j
+    
+        with open(file_name, 'a') as file:
+            file.write(f"Alignment Result {ind+1} with Score ({optimal_score}):\n" + ''.join(aligned_protein1) + '\n' + ''.join(aligned_protein2) + '\n'+ "------------------" + '\n')
+
+def plot_needleman_wunsch(protein1, protein2, scoring_scheme, file_name=None):
     """
     Plot the Needleman-Wunsch scoring matrix and save the alignment result if specified.
 
@@ -15,19 +50,15 @@ def plot_needleman_wunsch(protein1, protein2, scoring_scheme):
     scores = needleman_wunsch(protein1, protein2, scoring_scheme)
     all_paths = find_all_paths(scores, protein1, protein2, scoring_scheme)
 
-    # Create the plot
     plt.figure(figsize=(7, 6))
     im = plt.imshow(scores, cmap="coolwarm", aspect='auto')
     
-    # Adjust colorbar size
     plt.colorbar(im, label='Alignment Score', shrink=0.75)
 
-    # Display the score values in each cell
     for i in range(scores.shape[0]):
         for j in range(scores.shape[1]):
             plt.text(j, i, f'{int(scores[i, j])}', ha='center', va='center', color='black', fontsize=10)
 
-    # Generate colors for each path
     colors = plt.cm.viridis(np.linspace(0, 1, len(all_paths)))
 
     # Plot each optimal path in a different color
@@ -38,21 +69,24 @@ def plot_needleman_wunsch(protein1, protein2, scoring_scheme):
     plt.xlabel("Protein 2", fontsize=14)
     plt.ylabel("Protein 1", fontsize=14)
 
-    # Move Protein 2 labels to the top
     plt.gca().xaxis.set_ticks_position('top')
     plt.gca().xaxis.set_label_position('top')
     plt.xticks(ticks=np.arange(1, len(protein2) + 1), labels=list(protein2), fontsize=12)
     plt.yticks(ticks=np.arange(1, len(protein1) + 1), labels=list(protein1), fontsize=12)
 
-    # Move legend outside of the plot
-    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0, ncol=2, borderpad=1)
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0, labelspacing=1.2)
 
-    # Show plot with layout adjustment
     plt.tight_layout()
     plt.show()
+    print(all_paths)
+
+    # report the alignment results.
+    if file_name:
+        save_alignment_to_file(protein1, protein2, all_paths, scores[-1,-1],file_name)
 
 
-protein1 = "CTATGAA"
-protein2 = "CTATTGACGTA"
+
+protein1 = "CTATTGACGTA"
+protein2 = "CTATGAA"
 scoring_scheme = {'match_score': 5, 'mismatch_penalty': -2, 'gap_penalty': -4}
-plot_needleman_wunsch(protein1, protein2, scoring_scheme)
+plot_needleman_wunsch(protein1, protein2, scoring_scheme, "alignment_result.txt")
